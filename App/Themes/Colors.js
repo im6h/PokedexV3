a7f7574fc12691c87bc358f57bc53dc7466eba78
tjjone98 <tzero1898@gmail.com>
2020-03-15 20:22:27 +0700

    feat[color]: add color Pokemon type

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -1,4 +1,8 @@
 const colors = {
+  /**
+   * color base app
+   *
+   */
   background: "rgba(0,0,0,0)",
   clear: "rgba(0,0,0,0)",
   facebook: "#3b5998",
@@ -23,13 +27,28 @@ const colors = {
   banner: "#5F3E63",
   text: "#E0D7E5",
   primaryColor: "#1fd092",
+
+  /**
+   * color for type of pokemon
+   *
+   */
   fireColor: "#F08030",
   grassColor: "#78C850",
   poisonColor: "#A040A0",
   steelColor: "#B8B8D0",
   rockColor: "#B8A038",
   psychicColor: "#F85888",
-  normalColor: "#A8A878"
+  normalColor: "#A8A878",
+  bugColor: "#A8B820",
+  darkColor: "#705848",
+  dragonColor: "#7038F8",
+  electricColor: "#F8D030",
+  fairyColor: "#EE99AC",
+  fightingColor: "#C03028",
+  flyingColor: "#A890F0",
+  ghostColor: "#705898",
+  grouncColor: "#E0C068",
+  iceColor: "#98D8D8"
 };
 
 export default colors;
